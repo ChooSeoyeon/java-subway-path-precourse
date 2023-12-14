350a8461bb391182b8f38f00596570d844b34eac
Choo <choo000407@naver.com>
2023-12-14 22:23:40 +0900

    feat(Controller): 에러 메시지를 출력하고 관리한다

--- src/main/java/subway/controller/SubwayPathController.java
+++ src/main/java/subway/controller/SubwayPathController.java
@@ -1,4 +1,36 @@
 package subway.controller;
 
+import java.util.function.Supplier;
+import subway.view.InputView;
+import subway.view.OutputView;
+
 public class SubwayPathController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public SubwayPathController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    private <T> T repeatUntilSuccessWithReturn(Supplier<T> supplier) {
+        while (true) {
+            try {
+                return supplier.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
+
+    private void repeatUntilSuccess(Runnable action) {
+        while (true) {
+            try {
+                action.run();
+                return;
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
+    }
 }

a79ba162fa91eb1772fb2caba5b98df9c250438c
Choo <choo000407@naver.com>
2023-12-14 23:12:21 +0900

    feat(InputView): 기능을 입력받는다

--- src/main/java/subway/view/InputView.java
+++ src/main/java/subway/view/InputView.java
@@ -1,4 +1,27 @@
 package subway.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private final Scanner scanner;
+
+    public InputView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String readFunction() {
+        String inputFunction = scanner.nextLine();
+        validateFunction(inputFunction);
+        return inputFunction;
+    }
+
+    private void validateFunction(String inputFunction) {
+        if (inputFunction.equals("1")) {
+            return;
+        }
+        if (inputFunction.equals("Q")) {
+            return;
+        }
+        throw new IllegalArgumentException("지원하지 않는 기능입니다.\n");
+    }
 }
